7c61761a2befcc30f01844b4b5e1372bf89d6b9f
James Turnbull <james@lovedthanlost.net>
2010-01-15 21:42:26 +1100

    Removed rrd for hardy

--- manifest.pp
+++ manifest.pp
@@ -61,7 +61,7 @@ class rubygems {
          Fedora  => [ "ruby-devel", "postgresql-devel",   "mysql-devel",        "sqlite",  "sqlite-devel",   "rrdtool-devel", "openldap-devel" ],
          CentOS  => [ "ruby-devel", "postgresql-devel",   "mysql-devel",        "sqlite",  "sqlite-devel",   "rrdtool-devel", "openldap-devel" ],
          Ubuntu  => $operatingsystemrelease ? {
-             "8.04" => [ "ruby1.8-dev",   "libpq-dev",          "libmysqlclient15-dev", "sqlite3", "libsqlite3-dev", "librrd-dev",    "libldap2-dev" ],
+             "8.04" => [ "ruby1.8-dev",   "libpq-dev",          "libmysqlclient15-dev", "sqlite3", "libsqlite3-dev",    "libldap2-dev" ],
              default => [ "ruby-dev",   "libpq-dev",          "libmysqlclient-dev", "sqlite3", "libsqlite3-dev", "librrd-dev",    "libldap2-dev" ],
          },
          Debian  => [ "ruby-dev",   "libpq-dev",          "libmysqlclient-dev", "sqlite3", "libsqlite3-dev", "librrd-dev",    "libldap2-dev" ],
