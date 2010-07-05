39655b2f525c5b99e8453b167a2e5eff1272eaec
James Turnbull <james@lovedthanlost.net>
2010-07-06 06:22:46 +1000

    Fixed apt-key exec

--- developerbootstrap/manifests/debian.pp
+++ developerbootstrap/manifests/debian.pp
@@ -18,6 +18,7 @@ class developerbootstrap::debian {
     "lenny_key_missing_for_some_reason":
       onlyif => "/usr/bin/test -f /usr/bin/apt-key",
       command => "/usr/bin/apt-key adv --keyserver wwwkeys.eu.pgp.net --recv-keys 9AA38DCD55BE302B && /usr/bin/apt-get update",
+      returns => [ 100, 0 ],
   }
 
 }
